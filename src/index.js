c9e3ac0123eaaa957cd25d77873f90dd375fd6e3
jizq <jizhengqistrongman@126.com>
2017-04-24 14:22:58 +0800

    VideoDetails component completed and change App component from function based component to class based component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, {Component} from "react";
 import ReactDom from "react-dom";
+import YTSearch from "youtube-api-search";
 import SearchBar from "./components/search_bar";
-
+import VideoDetails from "./components/video_details";
 const API_KEY = "AIzaSyAF5rZweLYfpB4rDeXyX2wXTgmDnv7Y5e0";
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
+class App extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+		this.videoSearch("skateboard");
+	}
+
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoDetails video={this.state.selectedVideo}/>
+			</div>
+		);
+	}
 };
 
 ReactDom.render(<App />, document.querySelector(".container"));
