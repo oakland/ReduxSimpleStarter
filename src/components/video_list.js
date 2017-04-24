5115fa79433edb8a1bab906755cb3e081940fa0a
jizq <jizhengqistrongman@126.com>
2017-04-24 14:42:22 +0800

    complete video_list_item.js and modify the css of components

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -3,10 +3,10 @@ import VideoListItem from "./video_list_item";
 
 const VideoList = ({videos}) => {
 	const videoItems = videos.map(video => {
-		return <VideoListItem />
+		return <VideoListItem video={video} key={video.etag} />
 	});
 	return (
-		<ul>
+		<ul className="col-md-4 list-group">
 			{videoItems}
 		</ul>
 	);
